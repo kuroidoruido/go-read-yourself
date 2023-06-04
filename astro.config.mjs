2658eb909179abbb20dd9627679a5a90efedd490
Anthony Pena <anthony.pena@outlook.fr>
2023-06-04 22:12:15 +0200

    feat: setup docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://press.k49.fr.nf",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
