1536edf3a15d6c3b52530fb287115720af1523e0
Anthony Pena <anthony.pena@outlook.fr>
2023-12-03 20:22:06 +0100

    feat: group post by tags

--- src/utils/compile.util.spec.ts
+++ src/utils/compile.util.spec.ts
@@ -11,12 +11,10 @@ Au moins une partie des puces Qualcomm envoie des infos d'elles même sans passe
 
 On parle souvent d'espionnage chinois mais l'espionnage américain on en parle ?
 
-
 ### [Comment Facebook et Instagram servent à orchestrer le trafic sexuel de mineurs](https://www.madmoizelle.com/comment-facebook-et-instagram-servent-a-orchestrer-le-trafic-sexuel-de-mineurs-1523717)
 \\#Réseaux-sociaux #Trafic-sexuel #Mineur #Pédophilie #Divers
 
 Sujet qui sort un peu de l'edito mais faites attention à votre entourage, en particulier les enfants et ados, par rapport aux réseaux sociaux qui sont exploités très fortement pour trafic de mineur avec des gros réseaux très bien orchestrés
-
 
 ### [Google Authenticator – Attention à la synchronisation des données 2FA !!](https://korben.info/google-authenticator-synchronisation-donnees-2fa.html)
 \\#Sécurité #2FA #Google
@@ -27,11 +25,89 @@ Pour rappel : les infos 2FA (typiquement le code OTP qu'on a avec Google Authent
 
 Bon bah plus qu'à trouver une autre app pour dégager Google Authentificator...
 
+### [La spécification ECMAScript 2023 pour JavaScript inclut de nouvelles méthodes pour les tableaux, notamment la possibilité de rechercher un élément dans un tableau à partir de la fin du tableau](https://javascript.developpez.com/actu/343360/La-specification-ECMAScript-2023-pour-JavaScript-inclut-de-nouvelles-methodes-pour-les-tableaux-notamment-la-possibilite-de-rechercher-un-element-dans-un-tableau-a-partir-de-la-fin-du-tableau/)
+\\#ECMAScript #JavaScript #TypeScript #Frontend #Backend
+
+La spec 2023 de JavaScript vient de tomber, on y trouve des petites nouveautés mais rien de bien fou. Mais y'a sans doute des choses qui sont des gros attendus pour les développeurs de framework
+`;
+    expect(actual).toEqual(expected);
+  });
+
+  it("should format correctly a news set with grouping by tags", () => {
+    const actual = compilePostsAsFormattedMarkdown(NEWS, {
+      byTags: ["Frontend", "Backend", "IA", "Sécurité", "Divers"],
+    });
+    const expected = `## Frontend
 
 ### [La spécification ECMAScript 2023 pour JavaScript inclut de nouvelles méthodes pour les tableaux, notamment la possibilité de rechercher un élément dans un tableau à partir de la fin du tableau](https://javascript.developpez.com/actu/343360/La-specification-ECMAScript-2023-pour-JavaScript-inclut-de-nouvelles-methodes-pour-les-tableaux-notamment-la-possibilite-de-rechercher-un-element-dans-un-tableau-a-partir-de-la-fin-du-tableau/)
 \\#ECMAScript #JavaScript #TypeScript #Frontend #Backend
 
 La spec 2023 de JavaScript vient de tomber, on y trouve des petites nouveautés mais rien de bien fou. Mais y'a sans doute des choses qui sont des gros attendus pour les développeurs de framework
+
+## Sécurité
+
+### [Smartphones With Popular Qualcomm Chip Secretly Share Private Information With US Chip-Maker](https://www.nitrokey.com/news/2023/smartphones-popular-qualcomm-chip-secretly-share-private-information-us-chip-maker)
+\\#Sécurité #Qualcomm #Vie-privée
+
+Au moins une partie des puces Qualcomm envoie des infos d'elles même sans passer par l'OS vers des serveurs de Qualcomm, le tout sans consentement.
+
+On parle souvent d'espionnage chinois mais l'espionnage américain on en parle ?
+
+### [Google Authenticator – Attention à la synchronisation des données 2FA !!](https://korben.info/google-authenticator-synchronisation-donnees-2fa.html)
+\\#Sécurité #2FA #Google
+
+Google vient de proposer la synchronisation via son compte Google des données de 2FA (Authentification à 2 Facteurs)... Et même pas en chiffrement bout en bout... Du génie !
+
+Pour rappel : les infos 2FA (typiquement le code OTP qu'on a avec Google Authentificator) c'est un moyen de prouver qu'on possède un objet (contrairement au mot de passe qui est une preuve de connaissance), si vous le synchronisez sur un cloud ça prouve plus rien du tout. Et si c'est fait sans le bon niveau de chiffrement, là ça va forcément finir par leak et ne plus servir à rien
+
+Bon bah plus qu'à trouver une autre app pour dégager Google Authentificator...
+
+## Divers
+
+### [Comment Facebook et Instagram servent à orchestrer le trafic sexuel de mineurs](https://www.madmoizelle.com/comment-facebook-et-instagram-servent-a-orchestrer-le-trafic-sexuel-de-mineurs-1523717)
+\\#Réseaux-sociaux #Trafic-sexuel #Mineur #Pédophilie #Divers
+
+Sujet qui sort un peu de l'edito mais faites attention à votre entourage, en particulier les enfants et ados, par rapport aux réseaux sociaux qui sont exploités très fortement pour trafic de mineur avec des gros réseaux très bien orchestrés
+`;
+    expect(actual).toEqual(expected);
+  });
+
+  it("should format correctly a news set with specified default group name", () => {
+    const actual = compilePostsAsFormattedMarkdown(NEWS, {
+      byTags: ["Frontend", "Sécurité"],
+      defaultGroupName: "Default Group",
+    });
+    const expected = `## Frontend
+
+### [La spécification ECMAScript 2023 pour JavaScript inclut de nouvelles méthodes pour les tableaux, notamment la possibilité de rechercher un élément dans un tableau à partir de la fin du tableau](https://javascript.developpez.com/actu/343360/La-specification-ECMAScript-2023-pour-JavaScript-inclut-de-nouvelles-methodes-pour-les-tableaux-notamment-la-possibilite-de-rechercher-un-element-dans-un-tableau-a-partir-de-la-fin-du-tableau/)
+\\#ECMAScript #JavaScript #TypeScript #Frontend #Backend
+
+La spec 2023 de JavaScript vient de tomber, on y trouve des petites nouveautés mais rien de bien fou. Mais y'a sans doute des choses qui sont des gros attendus pour les développeurs de framework
+
+## Sécurité
+
+### [Smartphones With Popular Qualcomm Chip Secretly Share Private Information With US Chip-Maker](https://www.nitrokey.com/news/2023/smartphones-popular-qualcomm-chip-secretly-share-private-information-us-chip-maker)
+\\#Sécurité #Qualcomm #Vie-privée
+
+Au moins une partie des puces Qualcomm envoie des infos d'elles même sans passer par l'OS vers des serveurs de Qualcomm, le tout sans consentement.
+
+On parle souvent d'espionnage chinois mais l'espionnage américain on en parle ?
+
+### [Google Authenticator – Attention à la synchronisation des données 2FA !!](https://korben.info/google-authenticator-synchronisation-donnees-2fa.html)
+\\#Sécurité #2FA #Google
+
+Google vient de proposer la synchronisation via son compte Google des données de 2FA (Authentification à 2 Facteurs)... Et même pas en chiffrement bout en bout... Du génie !
+
+Pour rappel : les infos 2FA (typiquement le code OTP qu'on a avec Google Authentificator) c'est un moyen de prouver qu'on possède un objet (contrairement au mot de passe qui est une preuve de connaissance), si vous le synchronisez sur un cloud ça prouve plus rien du tout. Et si c'est fait sans le bon niveau de chiffrement, là ça va forcément finir par leak et ne plus servir à rien
+
+Bon bah plus qu'à trouver une autre app pour dégager Google Authentificator...
+
+## Default Group
+
+### [Comment Facebook et Instagram servent à orchestrer le trafic sexuel de mineurs](https://www.madmoizelle.com/comment-facebook-et-instagram-servent-a-orchestrer-le-trafic-sexuel-de-mineurs-1523717)
+\\#Réseaux-sociaux #Trafic-sexuel #Mineur #Pédophilie #Divers
+
+Sujet qui sort un peu de l'edito mais faites attention à votre entourage, en particulier les enfants et ados, par rapport aux réseaux sociaux qui sont exploités très fortement pour trafic de mineur avec des gros réseaux très bien orchestrés
 `;
     expect(actual).toEqual(expected);
   });
--- src/utils/compile.util.ts
+++ src/utils/compile.util.ts
@@ -1,5 +1,40 @@
-export function compilePostsAsFormattedMarkdown(posts: NewsEntry[]) {
-  return posts.sort(compareCreationDate).map(formatOneNews).join("\n\n");
+import { isDefined } from "./fp.util";
+
+interface Options {
+  byTags?: string[];
+  defaultGroupName?: string;
+}
+
+export function compilePostsAsFormattedMarkdown(
+  posts: NewsEntry[],
+  { byTags = [], defaultGroupName = "" }: Options = {}
+) {
+  const postsSortedByCreationDate = posts.sort(compareCreationDate);
+  const postsGroupedByTags = postsSortedByCreationDate
+    .map((post): [string, NewsEntry] => [
+      byTags.find((tag) => post.tags.includes(tag)) ?? defaultGroupName,
+      post,
+    ])
+    .reduce((groups, [tag, post]) => {
+      if (!(tag in groups)) {
+        groups[tag] = [];
+      }
+      groups[tag]?.push(post);
+      return groups;
+    }, {} as { [tag: string]: NewsEntry[] });
+
+  const groups = Object.keys(postsGroupedByTags);
+  if (groups.length === 1) {
+    return formatAll(postsGroupedByTags[groups[0]!]!);
+  }
+  return [...byTags, defaultGroupName]
+    .filter((tag) => isDefined(postsGroupedByTags[tag]))
+    .map((group) => `## ${group}\n\n${formatAll(postsGroupedByTags[group]!)}`)
+    .join("\n");
+}
+
+function formatAll(posts: NewsEntry[]) {
+  return posts.map(formatOneNews).join("\n");
 }
 
 function formatOneNews(news: NewsEntry) {
