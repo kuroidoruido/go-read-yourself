cf63ee902997a52968919de5becd4505f404965c
Anthony Pena <anthony.pena@outlook.fr>
2023-12-31 15:04:21 +0100

    chore: upgrade to Astro 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
   output: "server",
   site: "https://press.k49.fr.nf",
   adapter: node({
